8391727c01b2657e02b9ef7da12c8486ef136580
Dee-muck <dimakbmstu@gmail.com>
2016-02-06 20:23:12 +0300

    added RMySQL

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##makeCacheMatrix stores input matrix in memory, also has 4 functions to handle 
+##it
 
+makeCacheMatrix <- function(x = matrix()) {
+        inverseM <- NULL
+        set <- function(y) {
+                x <<- y
+                inverseM <<- NULL
+        }
+        get <- function() x
+        setInverseM <- function(NewInverseM) inverseM <<- NewInverseM
+        getInverseM <- function() inverseM
+        list(set = set, get = get,
+             setInverseM = setInverseM,
+             getInverseM = getInverseM)
 }
 
 
 ## Write a short comment describing this function
 
+##cacheSolve checks whether we have pre-calculated incerse matrix, if so -
+##load it from cache, otherwise - calcuate inverse matrix and put it into cache
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverseM <- x$getInverseM()
+        if(!is.null(inverseM)) {
+                message("getting cached `inverse Matrix")
+                return(inverseM)
+        }
+        data <- x$get()
+        inverseM <- solve(data, ...)
+        x$setInverseM(inverseM)
+        inverseM
 }
